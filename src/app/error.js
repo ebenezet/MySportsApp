727a7992553fcb29969abef46a9eef245338bc77
ebenezet <ebenezer.t@hotmail.fr>
2025-07-28 11:29:18 +0100

    styling home page

--- src/app/error.js
+++ src/app/error.js
@@ -2,10 +2,14 @@
 
 export default function Error({ error, reset }) {
   return (
-    <div>
-      <h2> We are sorry: something went wrong on this page!</h2>
-      <p>{error.message}</p>
-      <button onClick={() => reset()}>Try again</button>
-    </div>
+    <html>
+      <body>
+        <div>
+          <h2> We are sorry: something went wrong on this page!</h2>
+          <p>{error.message}</p>
+          <button onClick={() => reset()}>Try again</button>
+        </div>
+      </body>
+    </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="home">
+    <div className="flex flex-box justify-center items-center bg-green-300 h-90">
       <h2> Welcome to My-Sports-App</h2>
     </div>
   );
